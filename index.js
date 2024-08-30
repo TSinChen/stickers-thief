259637c8850e7b50ce0fbeb3612000d6fddc0166
TSinChen <sinin60076@gmail.com>
2024-08-30 11:48:22 +0800

    style: format

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const MENU_ITEM_NAME = '這個貼圖我偷走了'
+const MENU_ITEM_NAME = "這個貼圖我偷走了";
 
 const MENU_SUB_ITEM_NAME = {
   downloadImage: {
-    id: 'downloadImage',
-    title: '下載',
+    id: "downloadImage",
+    title: "下載",
   },
   copyImage: {
-    id: 'copyImage',
-    title: '複製',
-  }
-}
+    id: "copyImage",
+    title: "複製",
+  },
+};
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: MENU_ITEM_NAME,
     title: MENU_ITEM_NAME,
-    contexts: ['all'],
-    documentUrlPatterns: ['https://store.line.me/stickershop/product/*'],
+    contexts: ["all"],
+    documentUrlPatterns: ["https://store.line.me/stickershop/product/*"],
   });
   chrome.contextMenus.create({
     id: MENU_SUB_ITEM_NAME.downloadImage.id,
@@ -31,49 +31,53 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (!tab?.id) return
+  if (!tab?.id) return;
 
   switch (info.menuItemId) {
     case MENU_SUB_ITEM_NAME.downloadImage.id:
       chrome.scripting?.executeScript({
         target: { tabId: tab.id },
         function: () => {
-          const target = window.getSelection().focusNode.parentElement
-          const backgroundImage = window.getComputedStyle(target.children[target.childElementCount - 1]).backgroundImage
-          const url = backgroundImage.match(/url\("(.+)"\)/)[1]
+          const target = window.getSelection().focusNode.parentElement;
+          const backgroundImage = window.getComputedStyle(
+            target.children[target.childElementCount - 1]
+          ).backgroundImage;
+          const url = backgroundImage.match(/url\("(.+)"\)/)[1];
           fetch(url)
-            .then(res => res.blob())
-            .then(blob => {
+            .then((res) => res.blob())
+            .then((blob) => {
               const url = window.URL.createObjectURL(blob);
-              const a = document.createElement('a');
-              a.style.display = 'none';
+              const a = document.createElement("a");
+              a.style.display = "none";
               a.href = url;
               a.download = `${window.document.title}.png`;
               document.body.appendChild(a);
               a.click();
               window.URL.revokeObjectURL(url);
-            })
-        }
+            });
+        },
       });
-      break
+      break;
     case MENU_SUB_ITEM_NAME.copyImage.id:
       chrome.scripting?.executeScript({
         target: { tabId: tab.id },
         function: () => {
-          const target = window.getSelection().focusNode.parentElement
-          const backgroundImage = window.getComputedStyle(target.children[target.childElementCount - 1]).backgroundImage
-          const url = backgroundImage.match(/url\("(.+)"\)/)[1]
+          const target = window.getSelection().focusNode.parentElement;
+          const backgroundImage = window.getComputedStyle(
+            target.children[target.childElementCount - 1]
+          ).backgroundImage;
+          const url = backgroundImage.match(/url\("(.+)"\)/)[1];
           fetch(url)
-            .then(res => res.blob())
-            .then(blob => {
-              navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })])
-            })
-        }
+            .then((res) => res.blob())
+            .then((blob) => {
+              navigator.clipboard.write([
+                new ClipboardItem({ "image/png": blob }),
+              ]);
+            });
+        },
       });
-      break
+      break;
     default:
-      break
+      break;
   }
-
 });
-
